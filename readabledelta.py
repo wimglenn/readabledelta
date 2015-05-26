c2eb1c1cb4d007f0ade024b4f2365b5b1f54600c
wim glenn <wim.glenn@melbourneit.com.au>
2015-05-26 15:48:22 +1000

    that should be a classmethod actually

--- readabledelta.py
+++ readabledelta.py
@@ -3,10 +3,20 @@ from __future__ import unicode_literals
 from datetime import timedelta
 
 
-def to_human_readable(dt):
-    return ''
+def to_human_readable(delta):
+    years, days = divmod(delta.days, 365)
+    hours, minutes = divmod(delta.seconds, 60*60)
+    minutes, seconds = divmod(minutes, 60)
+    microseconds = delta.microseconds
+    result = ('')
+    return result
 
 
 class readabledelta(timedelta):
+
+    @classmethod
+    def from_timedelta(cls, dt):
+        return cls(days=dt.days, seconds=dt.seconds, microseconds=dt.microseconds)
+
     def __str__(self):
         return to_human_readable(self)
--- test_readabledelta.py
+++ test_readabledelta.py
@@ -1,4 +1,16 @@
 from __future__ import unicode_literals
 
+from datetime import timedelta
+from unittest import TestCase
+
 from readabledelta import readabledelta
 
+
+class TestReadableDelta(TestCase):
+
+    def setUp(self):
+        self.dt_original = timedelta(days=400, hours=5, minutes=6, seconds=7, microseconds=8)
+        self.dt_readable = readabledelta.from_timedelta(self.dt_original)
+
+    def test_is_a_timedelta(self):
+        self.assertIsInstance(self.dt_readable, timedelta)
