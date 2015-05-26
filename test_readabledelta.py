5bc94e710ec001968d11704014209a53a5e8ae2d
wim glenn <wim.glenn@melbourneit.com.au>
2015-05-26 15:52:56 +1000

    more tests

--- test_readabledelta.py
+++ test_readabledelta.py
@@ -10,7 +10,14 @@ class TestReadableDelta(TestCase):
 
     def setUp(self):
         self.dt_original = timedelta(days=400, hours=5, minutes=6, seconds=7, microseconds=8)
-        self.dt_readable = readabledelta.from_timedelta(self.dt_original)
+        self.dt_readable = readabledelta(days=400, hours=5, minutes=6, seconds=7, microseconds=8)
 
     def test_is_a_timedelta(self):
         self.assertIsInstance(self.dt_readable, timedelta)
+
+    def test_has_equality_with_original(self):
+        self.assertEqual(self.dt_readable, self.dt_original)
+
+    def test_instantiate_from_classmethod(self):
+        dt_readable_from_dt_original = readabledelta.from_timedelta(self.dt_original)
+        self.assertEqual(dt_readable_from_dt_original, self.dt_readable)
