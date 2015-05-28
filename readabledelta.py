ae4d8457a2f60695a788da42e0781ed0549025cb
wim glenn <wim.glenn@melbourneit.com.au>
2015-05-28 12:17:22 +1000

    version numbering

--- readabledelta.py
+++ readabledelta.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 from datetime import timedelta
 
+__version__ = '0.0.1'
+
 
 class readabledelta(timedelta):
 
--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from __future__ import unicode_literals
 
 from setuptools import setup
 
+from readabledelta import __version__  # simple solution because we have no 3rd party dependencies
+
 with open('README.rst') as f:
     long_description = f.read()
 
 setup(
     name='readabledelta',
-    version='0.0.1-dev',
+    version=__version__,
     description='Human readable modification of timedelta',
     long_description=long_description,
     author='Wim Glenn',
